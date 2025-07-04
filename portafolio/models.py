4ca10d004686c2ac2d263ae50633f3083565eaaa
emileth <maryelisberrios42@gmail.com>
2025-07-04 00:15:40 -0400

    Versión del portafolio con Django y base de datos activa

--- portafolio/models.py
+++ portafolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Proyecto(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha = models.DateField()
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.titulo
